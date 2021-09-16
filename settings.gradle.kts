c9719950a709f6acc4a085bf4262df402c6c08c3
Matthias Geisler <matthias.geisler@data4life.care>
2021-09-16 11:50:32 +0200

    Bump Gradle Enterprise Plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
 includeBuild("gradlePlugin/test-util-dependency")
 
 plugins {
-    id("com.gradle.enterprise") version("3.4.1")
+    id("com.gradle.enterprise") version("3.7")
 }
 
 include(
