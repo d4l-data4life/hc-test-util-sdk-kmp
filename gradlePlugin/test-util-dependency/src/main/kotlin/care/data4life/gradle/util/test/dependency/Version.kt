20cfb321608774a6ef45a85db2008842cc715c29
Matthias Geisler <matthias.geisler@data4life.care>
2022-04-22 11:27:30 +0200

    Bump Kotlin

--- gradlePlugin/test-util-dependency/src/main/kotlin/care/data4life/gradle/util/test/dependency/Version.kt
+++ gradlePlugin/test-util-dependency/src/main/kotlin/care/data4life/gradle/util/test/dependency/Version.kt
@@ -26,7 +26,7 @@ object Version {
     /**
      * https://github.com/JetBrains/kotlin
      */
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.6.10"
 
     /**
      * https://github.com/Kotlin/kotlinx.coroutines
